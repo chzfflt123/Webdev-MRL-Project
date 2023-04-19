27f7fa8706d520e05ac275dfb2bfa503bb0ad827
aekmekci72 <anna.ekmekci@gmail.com>
2023-04-19 09:37:04 -0400

    .... i apologize in advance

--- app.js
+++ app.js
@@ -11,18 +11,41 @@ app.get("/abstract",(req,res)=>{
     res.sendFile(__dirname+"/abstract.html");
 });
 
+app.get("/credits",(req,res)=>{
+    res.sendFile(__dirname+"/credits.html");
+});
+
+app.get("/intro",(req,res)=>{
+    res.sendFile(__dirname+"/intro.html");
+});
+
+app.get("/intro/riskconditions",(req,res)=>{
+    res.sendFile(__dirname+"/riskconditions.html");
+});
+
+app.get("/intro/riskconditions/confirmrisk",(req,res)=>{
+    res.sendFile(__dirname+"/confirmrisk.html");
+});
+
+app.get("/intro/riskconditions/confirmrisk/symptoms",(req,res)=>{
+    res.sendFile(__dirname+"/symptoms.html");
+});
+
+app.get("/intro/riskconditions/confirmrisk/symptoms/confirmsymptoms",(req,res)=>{
+    res.sendFile(__dirname+"/contirmsymptoms.html");
+});
+
+app.get("/intro/riskconditions/confirmrisk/symptoms/confirmsymptoms/results",(req,res)=>{
+    res.sendFile(__dirname+"/results.html");
+});
+
+
+
+
+
+
+
 // start the server
 app.listen(port, () => {
     console.log(`App server listening on ${port}`);
 })
-
-// define a route for the assignment list page 
-app.get("/assignments",(req,res)=>{
-    res.send("<h1>This is the assignments page</h1>");
-})
-
-// define a route for the assignment detail page 
-app.get("/assignments/details",(req,res)=>{
-    res.send("<h1>This is the assignment detail page</h1>");
-})
-
