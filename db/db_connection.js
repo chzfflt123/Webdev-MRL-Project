9f132f871574032545d9ec2dac85f13c57a9bd11
aekmekci72 <anna.ekmekci@gmail.com>
2023-05-04 15:55:18 -0400

    more db env stuff \(>_<)/

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql2')
+const dotenv = require('dotenv')
+
+// const dbConfig = {
+//     host: "<hostname>",
+//     port: 3306,
+//     user: "<username>",
+//     password: "<password>",
+//     database: "<schema>",
+//     connectionTimeout: 10000
+// }
 
 const dbConfig = {
-    host: "<hostname>",
-    port: 3306,
-    user: "<username>",
-    password: "<password>",
-    database: "<schema>",
-    connectionTimeout: 10000
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT || "10000")
 }
 
 const connection = mysql.createConnection(dbConfig);
