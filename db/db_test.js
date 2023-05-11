8dd26bb43b7ec8fd564ac8744a2eb08bb9588d88
aekmekci72 <anna.ekmekci@gmail.com>
2023-05-11 15:40:33 -0400

    sadness

--- db/db_test.js
+++ db/db_test.js
@@ -1,9 +1,12 @@
 const db = require("./db_connection")
 
-db.execute('SELECT 1 + 1 AS solution', (erorr, results) => {
-    if (error) throw error;
-    console.log(results);
-})
+db.execute('SELECT 1 + 1 AS solution', 
+    (error, results) => {
+        if (error)
+            throw error;
+        console.log(results);
+    }
+);
 
 db.end();
 
