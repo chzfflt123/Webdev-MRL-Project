9ba0c8b7cd270546e8f17cf3fd9d2cf72f574b4e
aekmekci72 <anna.ekmekci@gmail.com>
2023-05-14 20:43:43 -0400

    more executing just because

--- db/db_init.js
+++ db/db_init.js
@@ -16,6 +16,13 @@ const insert_patient_sql = `
         (?, ?, ?, ?, ?);
 `
 
-db.execute(insert_patient_sql, [1, 14, "2008-05-19", "4", "100"]);
+db.execute(insert_patient_sql, [1, 14, "2008-05-19", "64", "105"]);
+db.execute(insert_patient_sql, [2, 3, "2021-02-11", "36", "60"]);
+db.execute(insert_patient_sql, [3, 27, "1995-12-07", "64", "123"]);
+db.execute(insert_patient_sql, [4, 50, "1972-10-23", "64", "135"]);
+db.execute(insert_patient_sql, [5, 104, "1919-04-30", "64", "112"]);
+
+
+
 
 db.end();
